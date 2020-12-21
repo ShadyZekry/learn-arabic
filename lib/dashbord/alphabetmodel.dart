be0bea1663013f655a1d818e475a79f53407aa72
fadyfayek11 <fayekfady24@gmail.com>
2020-12-21 22:05:35 +0200

    third

---
+++ lib/dashbord/alphabetmodel.dart
@@ -0,0 +1,40 @@
+//model
+class Alphabet {
+  const Alphabet({this.title, this.id});
+  final String title;
+  final int id;
+}
+
+
+const List<Alphabet> letter = const <Alphabet>[
+  const Alphabet(title: 'أ', id: 1),
+  const Alphabet(title: 'ب', id: 0),
+  const Alphabet(title: 'ت', id: 0),
+  const Alphabet(title: 'ث', id: 1),
+  const Alphabet(title: 'ج', id: 0),
+  const Alphabet(title: 'ح',id: 0),
+  const Alphabet(title: 'خ', id: 1),
+  const Alphabet(title: 'د', id: 0),
+  const Alphabet(title: 'ذ', id: 1),
+  const Alphabet(title: 'ر', id: 0),
+  const Alphabet(title: 'ز', id: 0),
+  const Alphabet(title: 'س', id: 1),
+  const Alphabet(title: 'ش', id: 0),
+  const Alphabet(title: 'ص',id: 0),
+  const Alphabet(title: 'ض', id: 1),
+  const Alphabet(title: 'ط', id: 0),
+  const Alphabet(title: 'ظ', id: 1),
+  const Alphabet(title: 'ع', id: 0),
+  const Alphabet(title: 'غ', id: 0),
+  const Alphabet(title: 'ف', id: 1),
+  const Alphabet(title: 'ق', id: 0),
+  const Alphabet(title: 'ك',id: 0),
+  const Alphabet(title: 'ل', id: 1),
+  const Alphabet(title: 'م', id: 0),
+  const Alphabet(title: 'ن', id: 1),
+  const Alphabet(title: 'ه', id: 0),
+  const Alphabet(title: 'و', id: 0),
+  const Alphabet(title: 'ي', id: 1),
+
+];
+
