bc5afbb4cd3b45dd9c970f135400f20cae3baa53
Shady <ShadyZekry75@gmail.com>
2020-12-21 23:26:36 +0200

    Add dashboard screen to routes

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_arabic/dashbord/dashbordscreen.dart';
 import 'package:learn_arabic/home_screen.dart';
 import 'package:learn_arabic/splash_screen.dart';
 
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => SplashScreen(),
         '/home_screen': (context) => HomeScreen(),
+        '/dashboard_screen': (context) => Edition(),
       },
     );
   }
--- lib/dashbord/dashbordscreen.dart
+++ lib/dashbord/dashbordscreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dashbord/alphabetmodel.dart';
+import 'package:learn_arabic/dashbord/alphabetmodel.dart';
 
 class Edition extends StatefulWidget {
   @override
@@ -10,22 +10,19 @@ class _EditionState extends State<Edition> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:GridView.count
-        (
-          crossAxisCount: 4,
-          children: List.generate(letter.length, (index){
-            return new Container(
-                child: SelectCard(alphabet: letter[index]),
-              );
-              }),
-              ),
-            );
+      body: GridView.count(
+        crossAxisCount: 4,
+        children: List.generate(letter.length, (index) {
+          return new Container(
+            child: SelectCard(alphabet: letter[index]),
+          );
+        }),
+      ),
+    );
   }
 }
 
-
 ////////////////////////
-
 
 class SelectCard extends StatelessWidget {
   const SelectCard({Key key, this.alphabet}) : super(key: key);
@@ -39,28 +36,30 @@ class SelectCard extends StatelessWidget {
         shadowColor: Colors.black,
         margin: EdgeInsets.all(10.0),
         //color: Colors.blue,
-        color: Color.fromRGBO(44, 89, 211,.8),
+        color: Color.fromRGBO(44, 89, 211, .8),
         shape: new RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(20.0)
-        ),
-          child: new Column(
-            children:<Widget> [
-              Align(
-                alignment: Alignment.center,
-                child :new Text(alphabet.title,
-                 textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 23.0,
-                    color: Colors.white,
-                  ),
+            borderRadius: new BorderRadius.circular(20.0)),
+        child: new Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: new Text(
+                alphabet.title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 23.0,
+                  color: Colors.white,
+                ),
               ),
             ),
-              alphabet.id == 0? Icon(Icons.lock_open,color: Color.fromRGBO(254, 194, 0,1).withOpacity(.7))
-               :Icon(Icons.lock,color: Color.fromRGBO(254, 194, 0,1).withOpacity(1)),
+            alphabet.id == 0
+                ? Icon(Icons.lock_open,
+                    color: Color.fromRGBO(254, 194, 0, 1).withOpacity(.7))
+                : Icon(Icons.lock,
+                    color: Color.fromRGBO(254, 194, 0, 1).withOpacity(1)),
           ],
-          ),
+        ),
       ),
     );
   }
-
 }
