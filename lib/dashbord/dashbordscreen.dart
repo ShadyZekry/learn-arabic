80fb1fc08d16505430d5d3d46b74fabbe9e76eb7
fadyfayek11 <fayekfady24@gmail.com>
2020-12-27 14:05:57 +0200

    progress bar

--- lib/dashbord/dashbordscreen.dart
+++ lib/dashbord/dashbordscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:learn_arabic/dashbord/alphabetmodel.dart';
+import 'alphabetmodel.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class Edition extends StatefulWidget {
   @override
@@ -10,9 +11,10 @@ class _EditionState extends State<Edition> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
+      body:GridView.count
+        (
         crossAxisCount: 4,
-        children: List.generate(letter.length, (index) {
+        children: List.generate(letter.length, (index){
           return new Container(
             child: SelectCard(alphabet: letter[index]),
           );
@@ -22,44 +24,36 @@ class _EditionState extends State<Edition> {
   }
 }
 
+
 ////////////////////////
+
 
 class SelectCard extends StatelessWidget {
   const SelectCard({Key key, this.alphabet}) : super(key: key);
   final Alphabet alphabet;
 
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: new Card(
-        elevation: 15.0,
-        shadowColor: Colors.black,
-        margin: EdgeInsets.all(10.0),
-        //color: Colors.blue,
-        color: Color.fromRGBO(44, 89, 211, .8),
-        shape: new RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(20.0)),
-        child: new Column(
-          children: <Widget>[
-            Align(
-              alignment: Alignment.center,
-              child: new Text(
-                alphabet.title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 23.0,
-                  color: Colors.white,
-                ),
-              ),
+      child: new Column(
+        children:<Widget> [ //progressbar circular
+          new CircularPercentIndicator(
+            radius: 70.0,
+            lineWidth: 10.0,
+            animation: true,
+            percent:double.parse(alphabet.id.toString())/2 , //number of right answers
+            center: new Text(
+              alphabet.title, //alphabet
+              style:
+              new TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
             ),
-            alphabet.id == 0
-                ? Icon(Icons.lock_open,
-                    color: Color.fromRGBO(254, 194, 0, 1).withOpacity(.7))
-                : Icon(Icons.lock,
-                    color: Color.fromRGBO(254, 194, 0, 1).withOpacity(1)),
-          ],
-        ),
+            circularStrokeCap: CircularStrokeCap.round,
+            progressColor: Colors.blue,
+          ),
+        ],
       ),
     );
   }
+
 }
