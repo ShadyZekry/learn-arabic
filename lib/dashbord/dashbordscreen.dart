5c5a32a8712391297d23bd566feb064e20dafad2
fadyfayek11 <fayekfady24@gmail.com>
2020-12-28 17:04:35 +0200

    final third screen

--- lib/dashbord/dashbordscreen.dart
+++ lib/dashbord/dashbordscreen.dart
@@ -11,6 +11,15 @@ class _EditionState extends State<Edition> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "DashBord"
+        ),
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back_ios),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
       body:GridView.count
         (
         crossAxisCount: 4,
@@ -20,14 +29,57 @@ class _EditionState extends State<Edition> {
           );
         }),
       ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              _ShowScoreDialog(context);
+            },
+            child: new Image.asset(
+              "assets/images/coins.png",
+              fit: BoxFit.contain,
+            )
+        ),
+        backgroundColor: Color.fromRGBO(35, 114, 163,1)
     );
   }
 }
 
 
+
+
 ////////////////////////
+int sore=120;
+//////score dialog//////////////
 
+ _ShowScoreDialog(BuildContext context) {
 
+   return showDialog(context: context,barrierDismissible: true, builder: (param){
+     return AlertDialog(
+       title: Text("Your score"),
+       content: Column(
+         mainAxisSize: MainAxisSize.min,
+         children: <Widget>[
+           Text(
+             "$sore",
+             style: TextStyle(
+                 fontSize: 32,
+                 fontWeight: FontWeight.bold
+             ) ,
+           )
+         ],
+       ),
+       actions: <Widget>[
+         FlatButton(
+             onPressed: ()=>Navigator.pop(context),
+             color: Colors.red,
+             child: Text('OK')),
+       ],
+
+     );
+   });
+
+}
+
+////grid view/////////
 class SelectCard extends StatelessWidget {
   const SelectCard({Key key, this.alphabet}) : super(key: key);
   final Alphabet alphabet;
@@ -36,24 +88,26 @@ class SelectCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: new Column(
-        children:<Widget> [ //progressbar circular
-          new CircularPercentIndicator(
-            radius: 70.0,
-            lineWidth: 10.0,
-            animation: true,
-            percent:double.parse(alphabet.id.toString())/2 , //number of right answers
-            center: new Text(
-              alphabet.title, //alphabet
-              style:
-              new TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: new Column(
+          children:<Widget> [ //progressbar circular
+            new CircularPercentIndicator(
+              radius: 70.0,
+              lineWidth: 10.0,
+              animation: true,
+              percent:double.parse(alphabet.id.toString())/2 , //number of right answers
+              center: new Text(
+                alphabet.title, //alphabet
+                style:
+                new TextStyle(fontWeight: FontWeight.bold, fontSize: 32.0,color:Colors.white),
+              ),
+              circularStrokeCap: CircularStrokeCap.round,
+              progressColor:Color.fromRGBO(94, 186, 125,1),
             ),
-            circularStrokeCap: CircularStrokeCap.round,
-            progressColor: Colors.blue,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
-
 }
